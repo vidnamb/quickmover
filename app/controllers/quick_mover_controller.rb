dfcc8c4c6d2b0175269f63bddc0b644f2865074a
Vidya Nambiar <vnambiar@andrew.cmu.edu>
2013-09-15 18:59:25 -0700

    Initial commit

---
+++ app/controllers/quick_mover_controller.rb
@@ -0,0 +1,4 @@
+class QuickMoverController < ApplicationController
+  def home
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quickmover::Application.config.secret_key_base = 'd588d59853d646c8de4a70f9e46ade8751bd736ca888722021513aaa7d3421fb59daf8cfc3bd0762ccf62f22b57ee3be8fbc25dc7524ab631a41d9e284af29ab'
